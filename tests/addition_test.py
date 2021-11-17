4eae8152522415aa208f5ffbb4d3d10120bea08f
sagedemage <salsaeed999@gmail.com>
2021-11-17 00:43:03 -0500

    Changed back the test_addition method to use the short way of implementing it.

--- tests/addition_test.py
+++ tests/addition_test.py
@@ -7,7 +7,5 @@ def test_addition():
     """Testing addition method"""
     # Arrange
     addition = Addition(1, (2,))
-    # Act
-    result = addition.get_result()
-    # Assert
-    assert result == 3
+    # Act and Assert
+    assert addition.get_result() == 3
