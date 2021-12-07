219ea21f28327bfa3af9e900378ad55ae3872c75
sagedemage <salsaeed999@gmail.com>
2021-12-06 22:34:44 -0500

    Made sure the calculator program follows closely to SOLID. I made sure the code is organized and clean to the best of my ability.

--- app/app.py
+++ app/app.py
@@ -1,19 +1,48 @@
-from flask import Flask
-from flask import render_template
+"""Web Application Program"""
+from flask import Flask, render_template, request, flash
+from calc.calculator import Calculator
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
 @app.route('/')
 def homepage():
+    """Display homepage"""
     return render_template('index.html')
 
 
-@app.route('/soc')
-def soc():
-    return render_template('soc.html')
+@app.route('/form', methods=['POST', 'GET'])
+def form():
+    """Display the form"""
+    if request.method == 'POST':
+        # name of the tag
+        value1 = request.form['value1']
+        value2 = request.form['value2']
+        operation = request.form['operation']
+        getattr(Calculator, operation)(value1, (value2,))
+        result = ""
+        if Calculator.get_last_calculation() is None:
+            flash("Division by Zero Error!", "error")
+            result = "None"
+            return render_template('result.html', value1=value1,
+                                   value2=value2, operation=operation, result=result)
+        else:
+            result = str(Calculator.get_last_calculation())
+            flash("Success!", "success")
+            return render_template('result.html', value1=value1,
+                                   value2=value2, operation=operation, result=result)
+    else:
+        return render_template('form.html')
 
 
-@app.route('/testing_tools')
-def testing_tools():
-    return render_template('testing_tools.html')
+@app.route('/result')
+def result_message():
+    """Display the result"""
+    value1 = request.args.get('value1')
+    value2 = request.args.get('value2')
+    operation = request.args.get('operation')
+    result = request.args.get('result')
+    message = operation + " of " + value1 + " " + value2 + " is " + result
+    flash('You were successfully logged in')
+    return message
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,141 +1,78 @@
 """Import the calculator class"""
-import pytest
 from calc.calculator import Calculator
 
 
-@pytest.fixture
-def fixture_clear_history():
-    """Function runs each time a test method runs"""
-    # pylint: disable=redefined-outer-name
-    Calculator.clear_history()
-
-
-def test_calculator_add(fixture_clear_history):
+def test_calculator_add():
     """Testing addition method"""
     # pylint: disable=unused-argument, redefined-outer-name
     # Adding two numbers
-    assert Calculator.add_number(1, (2,)) == 3
+    assert Calculator.addition(1, (2,)) == 3
 
 
-def test_calculator_add_three_values(fixture_clear_history):
+def test_calculator_add_three_values():
     """Testing addition method with three values"""
     # pylint: disable=unused-argument, redefined-outer-name
 
     # Adding more than two numbers
-    assert Calculator.add_number(2, (4, 6)) == 12
+    assert Calculator.addition(2, (4, 6)) == 12
 
 
-def test_calculator_subtract(fixture_clear_history):
+def test_calculator_subtract():
     """Testing subtraction method"""
     # pylint: disable=unused-argument, redefined-outer-name
 
     # Subtracting two numbers
-    assert Calculator.subtract_number(8, (2,)) == 6
+    assert Calculator.subtraction(8, (2,)) == 6
 
 
-def test_calculator_subtract_three_values(fixture_clear_history):
+def test_calculator_subtract_three_values():
     """Testing subtraction method for three values"""
     # pylint: disable=unused-argument, redefined-outer-name
 
     # Subtracting three values
-    assert Calculator.subtract_number(16, (4, 2)) == 10
+    assert Calculator.subtraction(16, (4, 2)) == 10
 
 
-def test_calculator_multiply(fixture_clear_history):
+def test_calculator_multiply():
     """Testing the multiplication method"""
     # pylint: disable=unused-argument, redefined-outer-name
 
     # Multiplying two numbers
-    assert Calculator.multiply_number(3, (2,)) == 6
+    assert Calculator.multiplication(3, (2,)) == 6
 
 
-def test_calculator_multiply_three_value(fixture_clear_history):
+def test_calculator_multiply_three_value():
     """Testing multiplication method with three values"""
     # pylint: disable=unused-argument, redefined-outer-name
 
     # Multiplying three values
-    assert Calculator.multiply_number(2, (3, 5)) == 30
+    assert Calculator.multiplication(2, (3, 5)) == 30
 
 
-def test_calculator_divide(fixture_clear_history):
+def test_calculator_divide():
     """Testing division method"""
     # pylint: disable=unused-argument, redefined-outer-name
     # Dividing two numbers
-    assert Calculator.divide_number(4, (2,)) == 2
+    assert Calculator.division(4, (2,)) == 2
 
 
-def test_calculator_divide_three_value(fixture_clear_history):
+def test_calculator_divide_three_value():
     """Testing division method by three values"""
     # pylint: disable=unused-argument, redefined-outer-name
 
     # Dividing three values
-    assert Calculator.divide_number(50, (5, 2)) == 5
+    assert Calculator.division(50, (5, 2)) == 5
 
 
-def test_calculator_divide_by_zero(fixture_clear_history):
+def test_calculator_divide_by_zero():
     """Testing division by zero"""
     # pylint: disable=unused-argument, redefined-outer-name
-    # Read in the csv file
 
     # Dividing by zero
-    assert Calculator.divide_number(5, (0,)) is None
-    assert Calculator.divide_number(5, (0, 0)) is None
+    assert Calculator.division(5, (0,)) is None
+    assert Calculator.division(5, (0, 0)) is None
 
 
-def test_calculator_history_static_property(fixture_clear_history):
-    """Testing the number of calculations in the history"""
-    # pylint: disable=unused-argument, redefined-outer-name
-    # Add calculations to history
-    Calculator.add_number(2, (2,))
-    Calculator.subtract_number(3, (1,))
-    Calculator.multiply_number(4, (5,))
-    Calculator.divide_number(10, (2,))
-    assert Calculator.count_history() == 4
-
-
-def test_calculator_get_first_element_in_history(fixture_clear_history):
-    """Testing getting the first result from the history"""
-    # pylint: disable=unused-argument, redefined-outer-name
-    # Add calculations to history
-    Calculator.add_number(2, (2,))
-    Calculator.subtract_number(3, (1,))
-    Calculator.multiply_number(4, (5,))
-    Calculator.divide_number(10, (2,))
-    assert Calculator.get_result_of_first_calculation_in_history() == 4
-
-
-def test_calculator_get_last_element_in_history(fixture_clear_history):
-    """Testing getting the last result from the history"""
-    # pylint: disable=unused-argument, redefined-outer-name
-    # Add calculations to history
-    Calculator.add_number(2, (2,))
-    Calculator.subtract_number(3, (1,))
-    Calculator.multiply_number(4, (5,))
-    Calculator.divide_number(10, (2,))
-    assert Calculator.get_result_of_last_calculation_in_history() == 5
-
-
-def test_calculator_delete_history(fixture_clear_history):
-    """Testing deleting an item in the history"""
-    # pylint: disable=unused-argument, redefined-outer-name
-    # Add calculations to history
-    Calculator.add_number(2, (2,))
-    Calculator.subtract_number(3, (1,))
-    Calculator.multiply_number(4, (5,))
-    Calculator.divide_number(10, (2,))
-    Calculator.delete_history(0)
-    # Checking that the element index went from index 1 to index 0
-    assert Calculator.history[0].get_result() == 2
-
-
-def test_calculator_clear_history(fixture_clear_history):
-    """Testing clearing the history"""
-    # pylint: disable=unused-argument, redefined-outer-name
-    # Add calculations to history
-    Calculator.add_number(2, (2,))
-    Calculator.subtract_number(3, (1,))
-    Calculator.multiply_number(4, (5,))
-    Calculator.divide_number(10, (2,))
-    Calculator.clear_history()
-    # Checking if the history is empty
-    assert Calculator.history == []
+def test_calculator_last_calculation():
+    """Testing getting the last calculation in history"""
+    assert Calculator.get_last_calculation() is None
